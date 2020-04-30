744bc84a7352f9361afab513be41f4c1fd19930f
Stanislav <stas-68@mail.ru>
2020-04-30 10:18:28 +0300

    modif main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 // Переделать в ДЗ
-let getRequest = (`${API}/getBasket.json`)
-.then(response => response.json())
-.catch(error => {
-  console.log(error);
-});
+let getRequest = (url, cb) => {
+   let xhr = new XMLHttpRequest();
+   xhr.open('GET', url, true);
+   xhr.onreadystatechange = () => {
+     if (xhr.readyState === 4) {
+       if (xhr.status !== 200) {
+         console.log('Error');
+       } else {
+         cb(xhr.responseText);
+       }
+     }
+   };
+   xhr.send();
+ };
 
 
 class ProductList {
