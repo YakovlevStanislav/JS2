8f28f8afa36038dfe99370795e410fdc0a9c313d
Stanislav <stas-68@mail.ru>
2020-04-30 15:15:42 +0300

    задание 3.3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 // Переделать в ДЗ
-let getRequest = (`${API}/getBasket.json`)
-.then(response => response.json())
-.catch(error => {
-  console.log(error);
-});
+let getRequest = (url, cb) => {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status !== 200) {
+        console.log('Error');
+      } else {
+        cb(xhr.responseText);
+      }
+    }
+  };
+  xhr.send();
+};
 
 
 class ProductList {
@@ -53,7 +62,7 @@ class ProductList {
 
 class ProductItem {
   constructor(product, img = 'https://placehold.it/200x150') {
-    this.title = product.title;
+    this.product_name = product.product_name;
     this.price = product.price;
     this.id = product.id;
     this.img = img;
@@ -63,7 +72,7 @@ class ProductItem {
     return `<div class="product-item" data-id="${this.id}">
                 <img src="${this.img}" alt="Some img">
                 <div class="desc">
-                    <h3>${this.title}</h3>
+                    <h3>${this.product_name}</h3>
                     <p>${this.price} \u20bd</p>
                     <button class="buy-btn">Купить</button>
                 </div>
